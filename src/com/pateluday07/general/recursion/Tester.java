189b2e53f770471cec780335b2e79db38300c3e7
pateluday07 <pateluday07@gmail.com>
2025-04-01 09:26:08 +0530

    find factorial

--- src/com/pateluday07/general/recursion/Tester.java
+++ src/com/pateluday07/general/recursion/Tester.java
@@ -3,6 +3,7 @@ package com.pateluday07.general.recursion;
 public class Tester {
 
     public static void main(String[] args) {
+        //find factorial
         FactorialRecursion factorialRecursion = new FactorialRecursion();
         System.out.println(factorialRecursion.findFactorial(5));
 
