391916910770b8c022b916bb9951234f598fc036
pateluday07 <pateluday07@gmail.com>
2025-04-03 08:32:05 +0530

    modify factorial

--- src/com/pateluday07/general/recursion/RecursionTester.java
+++ src/com/pateluday07/general/recursion/RecursionTester.java
@@ -1,11 +1,13 @@
 package com.pateluday07.general.recursion;
 
-public class Tester {
+public class RecursionTester {
 
     public static void main(String[] args) {
         //find factorial
+        int i = 5;
         FactorialRecursion factorialRecursion = new FactorialRecursion();
-        System.out.println(factorialRecursion.findFactorial(5));
+        System.out.println("Find Factorial of Number: " + i);
+        System.out.println("Factorial: " + factorialRecursion.findFactorial(i));
 
     }
 }
