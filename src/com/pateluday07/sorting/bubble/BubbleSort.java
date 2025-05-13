66c85ab47088a356ddc39e167f9179f57b7fd7fb
pateluday07 <pateluday07@gmail.com>
2025-05-13 08:48:14 +0530

    add isSwapped check

--- src/com/pateluday07/sorting/bubble/BubbleSort.java
+++ src/com/pateluday07/sorting/bubble/BubbleSort.java
@@ -4,13 +4,17 @@ public class BubbleSort {
 
     public void sort(int[] arr) {
         for (int i = arr.length - 1; i > 0; i--) {
+            boolean isSwapped = false;
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    isSwapped = true;
                 }
             }
+            if (!isSwapped)
+                break;
         }
     }
 
