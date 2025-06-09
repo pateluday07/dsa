a046e9416122d61af2f7d371fd2e40290dee5b0e
pateluday07 <pateluday07@gmail.com>
2025-06-09 07:51:15 +0530

    correction in selection sort

--- src/com/pateluday07/sorting/selection/SelectionSort.java
+++ src/com/pateluday07/sorting/selection/SelectionSort.java
@@ -3,9 +3,9 @@ package com.pateluday07.sorting.selection;
 public class SelectionSort {
 
     public void sort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length -1; i++) {
             int minValueIndex = i;
-            for (int j = i; j < arr.length; j++) {
+            for (int j = i+1; j < arr.length; j++) {
                 if (arr[j] < arr[minValueIndex])
                     minValueIndex = j;
             }
