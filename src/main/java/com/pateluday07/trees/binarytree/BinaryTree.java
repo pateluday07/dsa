20a5949fa3a91c0342e3f6d204d13188473461b6
pateluday07 <pateluday07@gmail.com>
2025-10-18 12:21:09 +0530

    binary tree

---
+++ src/main/java/com/pateluday07/trees/binarytree/BinaryTree.java
@@ -0,0 +1,28 @@
+package com.pateluday07.trees.binarytree;
+
+public class BinaryTree {
+
+    private Node root;
+
+    public BinaryTree() {
+    }
+
+    public BinaryTree(Node root) {
+        this.root = root;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    @Override
+    public String toString() {
+        return "BinaryTree{" +
+                "root=" + root +
+                '}';
+    }
+}
---
+++ src/main/java/com/pateluday07/trees/binarytree/Node.java
@@ -0,0 +1,47 @@
+package com.pateluday07.trees.binarytree;
+
+public class Node {
+
+    private int value;
+    private Node leftChild;
+    private Node rightChild;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public Node(int value, Node leftChild, Node rightChild) {
+        this.value = value;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Node rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
